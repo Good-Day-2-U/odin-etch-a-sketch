75dab8609fe81633d714718036cabc3cbc882717
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-10-11 15:02:09 -0700

    Created prompt for window size Need to find way to remove old div squares

--- script.js
+++ script.js
@@ -3,41 +3,63 @@ let body = document.querySelector("body");
 
 let newDiv = document.createElement("div");
 
+body.appendChild(newDiv);
+newDiv.classList.add("board");
+
 
 let newDivInside;
 let newDivColumn;
 
 
-let gridInput = 42;
+let sizeButton = document.querySelector(".sizeButton")
+
+sizeButton.addEventListener("click", sizeClicked)
+
+let boardSize;
+
+function sizeClicked() {
+  let boardSize = prompt("Please enter a board size!")
+  gridInput = boardSize
+  mkBoard()
+}
+
+
+let gridInput;
+
+
+if (boardSize >= 1 || boardSize <= 100 ) {
+  let temp = boardSize;
+  gridInput = temp
+}
 
 
 
 
-body.appendChild(newDiv);
-newDiv.classList.add("board");
 
+let mkBoard = function makeGameBoard() {
 
+  // newDivColumn.remove()
 
-
-for (let i = 0; i < gridInput; i ++){
-  
-  let columnCount = gridInput
-
-  let newDivInside = document.createElement("div")
-  newDiv.appendChild(newDivInside)
-  newDivInside.classList.add("inside")
-
-  for (let i = 0; i < columnCount; i ++){
-  
-    let newDivColumn = document.createElement("div")
+  for (let i = 0; i < gridInput; i ++){
     
-    newDivInside.appendChild(newDivColumn)
-    newDivColumn.classList.add("column")
-    newDivColumn.classList.add("drawn")
+    let columnCount = gridInput
 
-    newDivColumn.addEventListener('mouseover', draw);
-  }
-};
+    let newDivInside = document.createElement("div")
+    newDiv.appendChild(newDivInside)
+    newDivInside.classList.add("inside")
+
+    for (let i = 0; i < columnCount; i ++){
+    
+      let newDivColumn = document.createElement("div")
+      
+      newDivInside.appendChild(newDivColumn)
+      newDivColumn.classList.add("column")
+      newDivColumn.classList.add("drawn")
+
+      newDivColumn.addEventListener('mouseover', draw);
+    }
+  };
+}
 
 
 let drawElement = document.querySelector(".column")
