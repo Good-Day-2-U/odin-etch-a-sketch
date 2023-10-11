16d85a8dee156d6105bdaea67de3c149df6bf10e
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-10-11 00:28:39 -0700

    Create and styled inner divs Done for tonight good day today

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ let newDivInside;
 let gridInput = 16
 
 
+let gridSquareHeight = 800/gridInput;
+let gridSquareWidth = 800/gridInput;
+
+
+
 body.appendChild(newDiv)
+newDiv.classList.add("board")
 
 
 
@@ -18,6 +24,13 @@ for (let i = 0; i < gridInput; i ++){
   
   newDiv.appendChild(newDivInside)
   newDivInside.classList.add("inside")
+  // newDivInside.style = "border: 1px solid red; Flex: 1"
+  // newDivInside.setAttribute("background-color", "blue")
+  // newDivInside.setAttribute("height:", gridSquareHeight + "px")
+  // newDivInside.setAttribute("width:", gridSquareWidth + "px")
 }
 
 let gridSquares = document.querySelector(".inside")
+
+
+newDiv.style = "border: 1px solid red; height: 800px; width: 800px; background-color: orange; Display: Flex"
